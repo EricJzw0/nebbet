9d064e1dadadf77b18c95621cd78284ee72a9b86
WANG HAO DA <whaodar@163.com>
2018-04-25 13:51:21 +0800

    Add django backend part etc.

---
+++ nebbet/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'nebbet.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^jet/', include('jet.urls', 'jet')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    # url(r'^api/', include('backend.urls')),
+]
